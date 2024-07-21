de20f5d8349ccf719b37d9250871a015939bdddd
juanbu17 <140201939+juanbu17@users.noreply.github.com>
2024-07-21 15:22:05 -0500

    clases category y Employee

---
+++ PythonProjectG3/CLASE6/biblioteca/Libreria/Employee.py
@@ -0,0 +1,49 @@
+
+
+
+
+class Employee():
+
+    rol_position = None
+    salary = None
+
+
+    def __init__(self,rol_position, salary):
+        self._rol_position = rol_position
+        self._salary = salary
+
+
+
+    @property
+    def rol_position(self):
+        return self._rol_position
+
+    @rol_position.setter
+    def salary(self, rol_position):
+        self._rol_position = rol_position
+
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, salary):
+        self._salary = salary
+
+
+    employees = []
+
+    def create_user(self):
+        employee = []
+        self._rol_position = input("Rol")
+        employee.append(self._rol_position)
+        self._salary = input("Salario")
+        employee.append(self._salary)
+
+        self.employees.append([self._rol_position,self._salary])
+
+
+    def list_employee_data(self):
+        for item in self.employees:
+            print(item)
---
+++ PythonProjectG3/CLASE6/biblioteca/Libreria/Test_employee.py
@@ -0,0 +1,3 @@
+from Libreria.Category import Category
+
+categoria = Category(None,None)
